14dc854872706bbcc5b9f5eb9c31f6173eb4f889
James Krolik <53527367+JimKrolik@users.noreply.github.com>
2021-05-28 14:53:44 -0500

    Update ClickUp-LowDiskCheck.ps1

--- ClickUp-LowDiskCheck.ps1
+++ ClickUp-LowDiskCheck.ps1
@@ -99,10 +99,7 @@ ForEach ($disk in $allFixedDisks) {
 
         $freeSpaceInGB = [math]::round(($disk.freespace / 1GB),2)
         $percentageFree = [int](($disk.freespace / $disk.size) * 100)
-        
-        write-host $driveLetter $freeSpaceInGB $percentageFree
-
-        $minimumGBTarget = 16
+       
         $minimumGBTarget = ($minimumGBTarget * 1024 * 1024 * 1024)
 
         #If we encounter a disk that is under the minimum size to check for, such as the System partition, just skip it entirely.
